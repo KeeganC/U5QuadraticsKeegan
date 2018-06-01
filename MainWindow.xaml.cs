a7304e0a6dfdd9aefd3e9c5a5cbeda124d24c530
KeeganC <36234333+KeeganC@users.noreply.github.com>
2018-06-01 13:50:40 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,10 +27,42 @@ namespace Drawing
     public partial class MainWindow : Window
     {
         double a, b, c, topP, topS, sqRtPre, sqRtPost, bottom, rootP, rootS, vertexX, vertexY;
+        double[,] myCells;
 
         public MainWindow()
         {
             InitializeComponent();
+            myCells = new double[20, 20];
+            string temp = "";
+            double offset = -4.6;
+            Rectangle[,] myRectangles = new Rectangle[20, 20];
+            MessageBox.Show(myCells.GetLength(1).ToString());
+            for (int col = 0; col < myCells.GetLength(0); col++)
+            {
+                for (int row = 0; row < myCells.GetLength(1); row++)
+                {
+                    myRectangles[row, col] = new Rectangle();
+                    myRectangles[row, col].Stroke = Brushes.DimGray;
+                    myRectangles[row, col].Fill = Brushes.White;
+                    if (this.Width < this.Height)
+                    {
+                        myRectangles[row, col].Width = this.Width / 30 - offset;
+                        myRectangles[row, col].Height = this.Width / 30 - offset;
+                    }
+                    else
+                    {
+                        myRectangles[row, col].Width = this.Height / 30 - offset;
+                        myRectangles[row, col].Height = this.Height / 30 - offset;
+                    }
+                    myCanvas.Children.Add(myRectangles[row, col]);
+                    Canvas.SetTop(myRectangles[row, col], row * myRectangles[row, col].Height -150);
+                    Canvas.SetLeft(myRectangles[row, col], col * myRectangles[row, col].Width -150);
+                    myCells[row, col] = 0;
+                    temp += myCells[row, col].ToString() + " ";
+                }
+
+                temp += "\n";
+            }
         }
 
         private void btnGo_Click(object sender, RoutedEventArgs e)
@@ -39,7 +71,7 @@ namespace Drawing
             b = Convert.ToDouble(txbInputB.Text);
             c = Convert.ToDouble(txbInputC.Text);
 
-            sqRtPre = (b*b)-(4*a*c);
+            sqRtPre = (b * b) - (4 * a * c);
             sqRtPost = Math.Sqrt(sqRtPre);
             topP = (b * (-1)) + sqRtPost;
             topS = (b * (-1)) - sqRtPost;
@@ -50,7 +82,7 @@ namespace Drawing
             vertexX = (rootS + rootP) / 2;
             vertexY = a * (vertexX * vertexX) + b * vertexX + c;
 
-            txbOutput.Text = "Entered value for a: "+ txbInputA.Text +"\r\nEntered value for b: "+ txbInputB.Text +"\r\nEntered value for c: "+ txbInputC.Text +"\r\nThe roots are "+ rootP.ToString() + " and " + rootS.ToString();
+            txbOutput.Text = "Entered value for a: " + txbInputA.Text + "\r\nEntered value for b: " + txbInputB.Text + "\r\nEntered value for c: " + txbInputC.Text + "\r\nThe roots are " + rootP.ToString() + " and " + rootS.ToString();
 
             //draw parabola
             Path p = new Path();
@@ -59,12 +91,12 @@ namespace Drawing
             PathGeometry pg = new PathGeometry();
             PathFigureCollection pfg = new PathFigureCollection();
             PathFigure pathFigure = new PathFigure();
-            pathFigure.StartPoint = new Point(rootP*15, 0);
+            pathFigure.StartPoint = new Point(rootP * 15, 0);
             pathFigure.Segments = new PathSegmentCollection();
 
             QuadraticBezierSegment quadraticBezierSegment = new QuadraticBezierSegment();
-            quadraticBezierSegment.Point1 = new Point(vertexX*15, -(vertexY*15));
-            quadraticBezierSegment.Point2 = new Point(rootS*15, 0);
+            quadraticBezierSegment.Point1 = new Point(vertexX * 15, -(vertexY * 15));
+            quadraticBezierSegment.Point2 = new Point(rootS * 15, 0);
 
             pathFigure.Segments.Add(quadraticBezierSegment);
 
